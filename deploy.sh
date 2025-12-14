4bfb7be50ebb557f1335ffb6bcb2e9de14f4864f
Петров Сергей Владимирович <spetrov@kr.digital>
2025-12-15 00:21:21 +0300

    .env file

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ sudo docker compose up -d
 sleep 30
 sudo docker ps
 sudo docker compose exec --user root php-fpm composer install --no-interaction
-sudo docker compose exec --user php-fpm php bin/console doctrine:migrations:migrate --no-interaction
+sudo docker compose exec --user root php-fpm php bin/console doctrine:migrations:migrate --no-interaction
 
 sudo chown www-data:www-data . -R
