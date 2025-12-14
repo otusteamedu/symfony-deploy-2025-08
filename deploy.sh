7aaa9c6266f00252b8e65969b487ead5adad4bdc
Петров Сергей Владимирович <spetrov@kr.digital>
2025-12-14 23:21:07 +0300

    .env file

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
-sudo rm /etc/nginx/sites-enabled/default
-
-
 sudo cp deploy/nginx.conf /etc/nginx/conf.d/demo.conf -f
 
-#sudo cp deploy/supervisor.conf /etc/supervisor/conf.d/demo.conf -f
-#sudo sed -i -- "s|%SERVER_NAME%|$1|g" /etc/nginx/conf.d/demo.conf
 sudo service nginx restart
+sudo cd /app
+
 sudo docker compose up -d --progress=plain
 sudo docker compose exec -T php-fpm php composer install -q
 sudo docker compose exec -T php-fpm php bin/console doctrine:migrations:migrate --no-interaction
