4e03d9d038327a72d37abf790609d6bd2177d915
Петров Сергей Владимирович <spetrov@kr.digital>
2025-12-14 23:33:15 +0300

    .env file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 sudo cp deploy/nginx.conf /etc/nginx/conf.d/demo.conf -f
 
 sudo service nginx restart
-sudo cd /app
 
 sudo docker compose up -d --progress=plain
 sudo docker compose exec -T php-fpm php composer install -q
